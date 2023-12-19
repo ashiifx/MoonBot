fc58be728bd9cb4d55847319f4bfd8a92c71ddcc
ashiifx <154308527+ashiifx@users.noreply.github.com>
2023-12-19 13:12:19 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -24,5 +24,5 @@ def get_current_time():
 
 
 def shorten_url(url):
-    site_url = f"https://tnshort.net/api?api=f12bdd3c5c118fdc7ab6dd26bbf360968923f379&url={url}&format=text"
+    site_url = f"https://instantearn.in/api?api=ab6ac843f27b946543dd2748a06ce6cd8dcc1040&url={url}&format=text"
     return str(requests.get(site_url).text)
